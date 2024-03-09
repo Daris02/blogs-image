794de578bf01baa8a91c06697199d84a752da2c7
Daris02 <hei.raymond.2@gmail.com>
2024-03-09 21:10:33 +0300

    feat: add header and footer

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='container'>
+      <div className="fixed bottom-0 w-full p-12 m-2 text-center rounded-sm bg-green-600">
+        <h1 className='text-2xl'>Footer</h1>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className='container'>
+      <div className='fixed top-0 w-full p-12 m-2  rounded-sm text-center bg-gray-600'>
+        <h1 className='text-2xl'>Header</h1>
+      </div>
+    </header>
+  )
+}
+
+export default Header
