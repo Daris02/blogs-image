3430dc99829d436d3db4bd58619a7dbd1dca2085
Daris02 <hei.raymond.2@gmail.com>
2024-03-11 14:31:14 +0300

    feat: update containte blog list to link

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -1,19 +1,22 @@
 'use client'
 
-import blogsList from '@/utils/data/blogList';
 import React from 'react'
-
+import { notFound, useRouter } from 'next/navigation';
+import blogsList from '@/utils/data/blogList';
 
 function BlogList() {
+  const router = useRouter();
 
   return (
     <div className='container py-2'>
       <h2>Blogs List</h2>
+      <ul>
       {
         blogsList.map((blog) => {
-          return <p key={blog.id}>Image {blog.id}</p>
+          return <li><a key={blog.id} onClick={() => router.push(`/blog/${blog.id}/image`)}>Image {blog.id}</a></li>
         })
       }
+      </ul>
     </div>
   )
 }
